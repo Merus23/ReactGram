d83707b2b92d904c5b3fd3e32ed9893f7402c1da
Merus23 <mateus.satira@gmail.com>
2023-11-16 18:51:32 -0300

    feat: Begining the frontend part. Cleaning the project.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter, Routes, Navigate, Route } from 'react-router-dom'
 
+
+import Home from './pages/Home/Home'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+function App() {
+  
   return (
-    <>
-      <h1>ReactGram</h1>
-            
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar/>
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/register' element={<Register/>}/>
+            </Routes>
+          </div>
+        <Footer/>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div>
+      <p>ReactGram &copy; 2023 </p>
+    </div>
+  )
+}
+
+export default Footer
