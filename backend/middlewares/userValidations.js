5277ec73e26df7b817406192f55acc01ea9dce6d
Merus23 <mateus.satira@gmail.com>
2023-11-22 15:33:13 -0300

    fix: problem with user validation

--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -5,7 +5,7 @@ const userCreateValidation = () => {
         body('name').isString().withMessage('O nome é obrigatório').isLength({min: 3}).withMessage('O nome deve ter no mínimo 3 caracteres'),
         body('email').isString().withMessage('O email é obrigatório').isEmail().withMessage('Insira um e-mail adequado'),
         body('password').isString().withMessage('A senha é obrigatória').isLength({min: 6}).withMessage('A senha deve ter no mínimo 6 caracteres'),
-        body('confirmpassword').isString().withMessage('A confirmação de senha é obrigatória').isLength({min: 6}).withMessage('A confirmação de senha deve ter no mínimo 6 caracteres').custom((value, {req}) => { if(value != req.body.password){
+        body('confirmPassword').isString().withMessage('A confirmação de senha é obrigatória').isLength({min: 6}).withMessage('A confirmação de senha deve ter no mínimo 6 caracteres').custom((value, {req}) => { if(value != req.body.password){
             throw new Error('As senhas não são iguais');}return true;
          }),
     ];
