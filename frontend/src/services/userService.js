ead73d8bbcfa5bfb1b4015ac3912074d3906d3b3
Merus23 <mateus.satira@gmail.com>
2023-11-30 16:28:12 -0300

    feat: update the login function

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,109 +1,54 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import userService from "./userService";
-
-const initialState = {
-  user: {},
-  error: false,
-  success: false,
-  loading: false,
-  message: null,
-};
-
-// Get user details, for edit data
-export const profile = createAsyncThunk(
-  "user/profile",
-  async (user, thunkAPI) => {
-    const token = thunkAPI.getState().auth.user.token;
-
-    const data = await userService.profile(user, token);
-
-    console.log(data);
-
-    return data;
-  }
-);
-
-// Update user details
-export const updateProfile = createAsyncThunk(
-  "user/update",
-  async (user, thunkAPI) => {
-    const token = thunkAPI.getState().auth.user.token;
-
-    const data = await userService.updateProfile(user, token);
-
-    // Check for errors
-    if (data.errors) {
-      return thunkAPI.rejectWithValue(data.errors[0]);
-    }
-
-    console.log(data);
-
-    return data;
-  }
-);
+import { api, requestConfig } from "../utils/config";
 
 // Get user details
-export const getUserDetails = createAsyncThunk(
-  "user/get",
-  async (id, thunkAPI) => {
-    const token = thunkAPI.getState().auth.user.token;
+const profile = async (data, token) => {
+  const config = requestConfig("GET", data, token);
 
-    const data = await userService.getUserDetails(id, token);
+  try {
+    const res = await fetch(api + "/users/profile", config)
+      .then((res) => res.json())
+      .catch((err) => err);
 
-    console.log(data);
-
-    return data;
+    return res;
+  } catch (error) {
+    console.log(error);
   }
-);
+};
 
-export const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    resetMessage: (state) => {
-      state.message = null;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(profile.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(profile.fulfilled, (state, action) => {
-        state.loading = false;
-        state.success = true;
-        state.error = null;
-        state.user = action.payload;
-      })
-      .addCase(updateProfile.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(updateProfile.fulfilled, (state, action) => {
-        state.loading = false;
-        state.success = true;
-        state.error = null;
-        state.user = action.payload;
-        state.message = "Usuário atualizado com sucesso!";
-      })
-      .addCase(updateProfile.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-        state.user = null;
-      })
-      .addCase(getUserDetails.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(getUserDetails.fulfilled, (state, action) => {
-        state.loading = false;
-        state.success = true;
-        state.error = null;
-        state.user = action.payload;
-      });
-  },
-});
+// Update user details
+const updateProfile = async (data, token) => {
+  const config = requestConfig("PUT", data, token, true);
 
-export const { resetMessage } = userSlice.actions;
-export default userSlice.reducer;
+  try {
+    const res = await fetch(api + "/users/", config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Get user details
+const getUserDetails = async (id) => {
+  const config = requestConfig("GET");
+
+  try {
+    const res = await fetch(api + "/users/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const userService = {
+  profile,
+  updateProfile,
+  getUserDetails,
+};
+
+export default userService;
