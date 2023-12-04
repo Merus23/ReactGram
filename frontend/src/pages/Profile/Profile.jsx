14f5a85afcdad804e45fa82fa7b682b4c30c040f
Merus23 <mateus.satira@gmail.com>
2023-12-04 15:21:58 -0300

    fix: bug the pages

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,141 +1,230 @@
-import "./EditProfile.css";
+import "./Profile.css";
 
 import { uploads } from "../../utils/config";
 
-// Hooks
-import { useEffect, useState } from "react";
+// components
+import Message from "../../components/Message";
+import { Link } from "react-router-dom";
+import { BsFillEyeFill, BsPencilFill, BsXLg } from "react-icons/bs";
+
+// hooks
+import { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
 // Redux
-import { profile, updateProfile, resetMessage } from "../../slices/userSlice";
-
-// Components
-import Message from "../../components/Message";
+import { getUserDetails } from "../../slices/userSlice";
+import {
+  getUserPhotos,
+  publishPhoto,
+  resetMessage,
+  deletePhoto,
+  updatePhoto,
+} from "../../slices/photoSlice";
 
 const Profile = () => {
+  const { id } = useParams();
+
   const dispatch = useDispatch();
 
-  const { user, message, error, loading } = useSelector((state) => state.user);
+  const { user, loading } = useSelector((state) => state.user);
+  const { user: userAuth } = useSelector((state) => state.auth);
+  const {
+    photos,
+    loading: loadingPhoto,
+    error: errorPhoto,
+    message: messagePhoto,
+  } = useSelector((state) => state.photo);
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [profileImage, setProfileImage] = useState("");
-  const [bio, setBio] = useState("");
-  const [previewImage, setPreviewImage] = useState("");
+  const [title, setTitle] = useState();
+  const [image, setImage] = useState();
+
+  const [editId, setEditId] = useState();
+  const [editImage, setEditImage] = useState();
+  const [editTitle, setEditTitle] = useState();
+
+  // New form and edit form refs
+  const newPhotoForm = useRef();
+  const editPhotoForm = useRef();
 
   // Load user data
   useEffect(() => {
-    dispatch(profile());
-  }, [dispatch]);
+    dispatch(getUserDetails(id));
+    dispatch(getUserPhotos(id));
+  }, [dispatch, id]);
 
-  // fill user form
-  useEffect(() => {
-    if (user) {
-      setName(user.name);
-      setEmail(user.email);
-      setBio(user.bio);
-    }
-  }, [user]);
+  // Reset component message
+  function resetComponentMessage() {
+    setTimeout(() => {
+      dispatch(resetMessage());
+    }, 2000);
+  }
 
-  const handleSubmit = async (e) => {
+  // Publish a new photo
+  const submitHandle = (e) => {
     e.preventDefault();
 
-    // Gather user data from states
-    const userData = {
-      name,
+    const photoData = {
+      title,
+      image,
     };
-
-    if (profileImage) {
-      userData.profileImage = profileImage;
-    }
-
-    if (bio) {
-      userData.bio = bio;
-    }
-
-    if (password) {
-      userData.password = password;
-    }
 
     // build form data
     const formData = new FormData();
 
-    const userFormData = Object.keys(userData).forEach((key) =>
-      formData.append(key, userData[key])
+    const photoFormData = Object.keys(photoData).forEach((key) =>
+      formData.append(key, photoData[key])
     );
 
-    formData.append("user", userFormData);
+    formData.append("photo", photoFormData);
 
-    await dispatch(updateProfile(formData));
+    dispatch(publishPhoto(formData));
 
-    setTimeout(() => {
-      dispatch(resetMessage());
-    }, 2000);
+    setTitle("");
+
+    resetComponentMessage();
   };
 
+  // change image state
   const handleFile = (e) => {
-    // image preview
     const image = e.target.files[0];
 
-    setPreviewImage(image);
-
-    // change image state
-    setProfileImage(image);
+    setImage(image);
   };
 
+  // Exclude an image
+  const handleDelete = (id) => {
+    dispatch(deletePhoto(id));
+
+    resetComponentMessage();
+  };
+
+  // Show or hide forms
+  function hideOrShowForms() {
+    newPhotoForm.current.classList.toggle("hide");
+    editPhotoForm.current.classList.toggle("hide");
+  }
+
+  // Show edit form
+  const handleEdit = (photo) => {
+    if (editPhotoForm.current.classList.contains("hide")) {
+      hideOrShowForms();
+    }
+
+    setEditId(photo._id);
+    setEditImage(photo.image);
+    setEditTitle(photo.title);
+  };
+
+  // Cancel editing
+  const handleCancelEdit = () => {
+    hideOrShowForms();
+  };
+
+  // Update photo title
+  const handleUpdate = (e) => {
+    e.preventDefault();
+
+    const photoData = {
+      title: editTitle,
+      id: editId,
+    };
+
+    dispatch(updatePhoto(photoData));
+
+    resetComponentMessage();
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <div id="edit-profile">
-      <h2>Edite seus dados</h2>
-      <p className="subtitle">
-        Adicione uma imagem de perfil, e conte mais um pouco sobre você...
-      </p>
-      {(user.profileImage || previewImage) && (
-        <img
-          className="profile-image"
-          src={
-            previewImage
-              ? URL.createObjectURL(previewImage)
-              : `${uploads}/users/${user.profileImage}`
-          }
-          alt={user.name}
-        />
+    <div id="profile">
+      <div className="profile-header">
+        {user.profileImage && (
+          <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+        )}
+        <div className="profile-description">
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </div>
+      </div>
+      {id === userAuth._id && (
+        <>
+          <div className="new-photo" ref={newPhotoForm}>
+            <h3>Compartilhe algum momento seu:</h3>
+            <form onSubmit={submitHandle}>
+              <label>
+                <span>Título para a foto:</span>
+                <input
+                  type="text"
+                  placeholder="Insira um título"
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title || ""}
+                />
+              </label>
+              <label>
+                <span>Imagem:</span>
+                <input type="file" onChange={handleFile} />
+              </label>
+              {!loadingPhoto && <input type="submit" value="Postar" />}
+              {loadingPhoto && (
+                <input type="submit" disabled value="Aguarde..." />
+              )}
+            </form>
+          </div>
+          <div className="edit-photo hide" ref={editPhotoForm}>
+            <p>Editando:</p>
+            {editImage && (
+              <img src={`${uploads}/photos/${editImage}`} alt={editTitle} />
+            )}
+            <form onSubmit={handleUpdate}>
+              <input
+                type="text"
+                onChange={(e) => setEditTitle(e.target.value)}
+                value={editTitle || ""}
+              />
+              <input type="submit" value="Atualizar" />
+              <button className="cancel-btn" onClick={handleCancelEdit}>
+                Cancelar edição
+              </button>
+            </form>
+          </div>
+          {errorPhoto && <Message msg={errorPhoto} type="error" />}
+          {messagePhoto && <Message msg={messagePhoto} type="success" />}
+        </>
       )}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Nome"
-          onChange={(e) => setName(e.target.value)}
-          value={name || ""}
-        />
-        <input type="email" placeholder="E-mail" disabled value={email || ""} />
-        <label>
-          <span>Imagem de Perfil:</span>
-          <input type="file" onChange={handleFile} />
-        </label>
-        <label>
-          <span>Bio:</span>
-          <input
-            type="text"
-            placeholder="Descrição do perfil"
-            onChange={(e) => setBio(e.target.value)}
-            value={bio || ""}
-          />
-        </label>
-        <label>
-          <span>Quer alterar sua senha?</span>
-          <input
-            type="password"
-            placeholder="Digite sua nova senha..."
-            onChange={(e) => setPassword(e.target.value)}
-            value={password || ""}
-          />
-        </label>
-        {!loading && <input type="submit" value="Atualizar" />}
-        {loading && <input type="submit" disabled value="Aguarde..." />}
-        {error && <Message msg={error} type="error" />}
-        {message && <Message msg={message} type="success" />}
-      </form>
+      <div className="user-photos">
+        <h2>Fotos publicadas:</h2>
+        <div className="photos-container">
+          {photos &&
+            photos.map((photo) => (
+              <div className="photo" key={photo._id}>
+                {photo.image && (
+                  <img
+                    src={`${uploads}/photos/${photo.image}`}
+                    alt={photo.title}
+                  />
+                )}
+                {id === userAuth._id ? (
+                  <div className="actions">
+                    <Link to={`/photos/${photo._id}`}>
+                      <BsFillEyeFill />
+                    </Link>
+                    <BsPencilFill onClick={() => handleEdit(photo)} />
+                    <BsXLg onClick={() => handleDelete(photo._id)} />
+                  </div>
+                ) : (
+                  <Link className="btn" to={`/photos/${photo._id}`}>
+                    Ver
+                  </Link>
+                )}
+              </div>
+            ))}
+          {photos.length === 0 && <p>Ainda não há fotos publicadas...</p>}
+        </div>
+      </div>
     </div>
   );
 };
